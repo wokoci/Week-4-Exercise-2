781c10ce74e501fb06550ec13b2d70f4e07a8597
Jeffrey Wokocha <Jeffrey.Wokocha@defra.onmicrosoft.com>
2025-01-28 10:20:37 +0000

    add public subnet association

--- vpc.tf
+++ vpc.tf
@@ -104,15 +104,16 @@ resource "aws_route_table" "jeff_Tf_RT" {
   }
 }
 
-resource "aws_route_table" "jeff_Tf_Private_RT" {
-  vpc_id = aws_vpc.jeff_Tf_vpc.id
+# create route table for public subnets 
+resource "aws_route_table_association" "jeff_Tf_RT_association_subnet1" {
+  subnet_id      = aws_subnet.jeff_Tf_subnet.id
+  route_table_id = aws_route_table.jeff_Tf_RT.id
 
-  route {
-    cidr_block = "10.0.0.0/16"
-    gateway_id = "local"
-  }
-
-  tags = {
-    Name = "${var.project_name}-jeff_Tf_route"
-  }
 }
+# create route table for public subnets 2
+resource "aws_route_table_association" "jeff_Tf_RT_association_subnet2" {
+  subnet_id      = aws_subnet.jeff_Tf_subnet2.id
+  route_table_id = aws_route_table.jeff_Tf_RT.id
+
+}
+
