5adf7ae2e417d62f0e200595b9be45347876144e
Jeffrey Wokocha <Jeffrey.Wokocha@defra.onmicrosoft.com>
2025-01-31 15:25:09 +0000

    add backend statefiles for dev and prod

--- 004_backend.tf
+++ 004_backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     bucket = "jeffrey-tf-state"
-    key    = "jeff/terraform.tfstate"
-    # key    = "jeff/dev/terraform.tfstate"
-    # key    = "jeff/prod/terraform.tfstate"
+    # key    = "jeff/terraform.tfstate"
+    key    = "dev/terraform.tfstate"
+    # key    = "prod/terraform.tfstate"
     region = "eu-west-1"
     dynamodb_table = "terraform-state-lock"
   }
