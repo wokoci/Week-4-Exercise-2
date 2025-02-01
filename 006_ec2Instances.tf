fdefc4eb7393b5a1b58cd3625cacb25644329018
Jeffrey Wokocha <Jeffrey.Wokocha@defra.onmicrosoft.com>
2025-02-01 23:31:21 +0000

    update key pair with new key localy generated

--- 006_ec2Instances.tf
+++ 006_ec2Instances.tf
@@ -1,7 +1,6 @@
 resource "aws_key_pair" "jeff_key_pair" {
   key_name   = "jeff-key"
-  public_key = file("./key_material.txt")
-  # public_key = file("~/.ssh/id_rsa.pub")
+  public_key = file("~/.ssh/id_rsa.pub")
   tags = {
     Name = "${var.project_name}-${var.environment}-jeff-key"
   }
@@ -9,12 +8,12 @@ resource "aws_key_pair" "jeff_key_pair" {
 
 
 resource "aws_instance" "Jumpbox_ec2_instance" {
-  ami             = "ami-0720a3ca2735bf2fa"
-  instance_type   = "t2.micro"
-  key_name        = aws_key_pair.jeff_key_pair.key_name
-  tenancy         = "default"
-  subnet_id       = aws_subnet.jeff_Tf_mgmt_subnet.id
-  security_groups = [aws_security_group.jump_box_SG.id]
+  ami                         = "ami-0720a3ca2735bf2fa"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.jeff_key_pair.key_name
+  tenancy                     = "default"
+  subnet_id                   = aws_subnet.jeff_Tf_mgmt_subnet.id
+  security_groups             = [aws_security_group.jump_box_SG.id]
   associate_public_ip_address = true
   tags = {
     Name = "${var.project_name}-${var.environment}-jeff-jumpbox_Instance"
